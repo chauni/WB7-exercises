e85a9cabfba968b461e7365afcb97170bf056a3f
Chauni <chaunidbrown@gmail.com>
2023-06-06 08:59:04 -0400

    json exercise 1

---
+++ json-placeholder/todo.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const toDoInputEl = document.getElementById('toDoId');
+const toDoBtnEl = document.getElementById('toDoBtn');
+const toDoIdEl = document.getElementById('toDoId');
+const toDoMessageDiv = document.getElementById('toDoMessage')
+
+toDoBtnEl.addEventListener('click', () => {
+    toDoMessageDiv.innerHTML = ''
+    fetch(`http://jsonplaceholder.typicode.com/todos/${toDoId.value}`).then(response => response.json()).then(data => {
+            toDoMessageDiv.innerHTML = `<h5>${data.title}</h5><hr> <p style="display: flex; justify-content: center;">${data.completed}</p>`
+        })
+})
